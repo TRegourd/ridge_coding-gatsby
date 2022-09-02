64e9c7dade24e96df4a8ef438e9eeaa99c61295a
Thomas Regourd <tom.reg@hotmail.fr>
2022-09-02 14:52:50 +0200

    init homepage from template

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,23 @@ module.exports = {
         createDemoPages: false,
         colorMode: false
       }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Ridge Coding Développement Web',
+        short_name: 'Ridge Coding',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#01998e',
+        display: 'minimal-ui',
+        icon: 'content/assets/favicon.png'
+      }
+    },
+    {
+      // ATTENTION: Match the theme name with the theme you're using
+      resolve: '@elegantstack/gatsby-theme-flexiblocks',
+      options: {}
     }
   ],
   // Customize your site metadata
